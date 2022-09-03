9f2d226ec4fa83846a4267ae3a9652a9027f99a2
Andrej Ceremnih <andrejceremnih@gmail.com>
2022-09-03 21:11:07 +0300

    most likely finished with 'Instruction'

--- screen/instruction.go
+++ screen/instruction.go
@@ -2,6 +2,7 @@ package screen
 
 import (
 	"eklase/state"
+	"fmt"
 	"image"
 	"image/color"
 	"image/png"
@@ -11,12 +12,57 @@ import (
 	"gioui.org/layout"
 	"gioui.org/op/clip"
 	"gioui.org/op/paint"
-	"gioui.org/text"
 	"gioui.org/unit"
 	"gioui.org/widget"
 	"gioui.org/widget/material"
 	"github.com/nfnt/resize"
 )
+
+func InstructionText(th *material.Theme, list *widget.List) func(gtx layout.Context) layout.Dimensions {
+	lightContrast := th.ContrastBg
+	lightContrast = color.NRGBA{A: 0xaa, R: 0x38, G: 0x38, B: 0x34}
+	darkContrast := th.ContrastBg
+	darkContrast = color.NRGBA{A: 0xaa, R: 0x2d, G: 0x2d, B: 0x2d}
+
+	texts1 := []string{"- ARROW LEFT (←)", "- ARROW RIGHT (→)", "- ARROW UP (↑)", "- ARROW DOWN (↓)"}
+	texts2 := []string{": MOVE THE FIGURE TO THE LEFT", ": MOVE THE FIGURE TO THE RIGHT", ": ROTATE THE FIGURE", ": SPEED UP THE FALL OF THE FIGURE"}
+
+	return func(gtx layout.Context) layout.Dimensions {
+		return material.List(th, list).Layout(gtx, 4, func(gtx layout.Context, index int) layout.Dimensions {
+			labelFn := func(size float32, s string) material.LabelStyle { // to change the color of the text
+				style := material.Label(th, unit.Dp(size), s)
+				style.Color = color.NRGBA{A: 0xff, R: 0xff, G: 0xff, B: 0xff}
+				return style
+			}
+
+			text1, text2 := texts1[index], texts2[index]
+
+			return layout.Stack{}.Layout(gtx,
+				layout.Expanded(func(gtx layout.Context) layout.Dimensions {
+					color := lightContrast
+					if index%2 == 0 {
+						color = darkContrast
+					}
+
+					max := image.Pt(gtx.Constraints.Max.X, gtx.Constraints.Min.Y)
+					paint.FillShape(gtx.Ops, color, clip.Rect{Max: max}.Op())
+					return layout.Dimensions{Size: gtx.Constraints.Min}
+				}),
+				layout.Stacked(rowInset(func(gtx layout.Context) layout.Dimensions {
+					return layout.Flex{}.Layout(gtx,
+						layout.Rigid(rowInset(labelFn(17, text1).Layout)),
+					)
+				})),
+				layout.Stacked(rowInset(func(gtx layout.Context) layout.Dimensions {
+					return layout.Flex{}.Layout(gtx,
+						layout.Rigid(rowInset(material.Body1(th, fmt.Sprintln("                                      ")).Layout)),
+						layout.Rigid(rowInset(labelFn(17, text2).Layout)),
+					)
+				})),
+			)
+		})
+	}
+}
 
 func Instruction(th *material.Theme, state *state.State) Screen {
 	var (
@@ -24,36 +70,28 @@ func Instruction(th *material.Theme, state *state.State) Screen {
 		addimage widget.Image
 	)
 	addimage.Src = drawPNG()
+	list := widget.List{List: layout.List{Axis: layout.Vertical}}
+
+	textRowLayout := InstructionText(th, &list)
 	return func(gtx layout.Context) (Screen, layout.Dimensions) {
 		widgetcolor := th.ContrastBg // to change the background color of the widget
 		widgetcolor.A, widgetcolor.R, widgetcolor.G, widgetcolor.B = 0xff, 0x00, 0x0a, 0x12
 		max := image.Pt(gtx.Constraints.Max.X, gtx.Constraints.Min.Y)
 		paint.FillShape(gtx.Ops, widgetcolor, clip.Rect{Max: max}.Op())
 
-		textRowLayout := func(gtx layout.Context) layout.Dimensions {
-			labelFn := func(size float32, s string) material.LabelStyle { // to change the color of the text
-				style := material.Label(th, unit.Dp(size), s)
-				style.Color = color.NRGBA{A: 0xff, R: 0xff, G: 0xff, B: 0xff}
-				return style
-			}
-			return layout.Flex{Axis: layout.Vertical, Spacing: layout.SpaceStart}.Layout(gtx,
-				layout.Rigid(rowInset(labelFn(21, "* INSTRUCTION TO MY 'TETRIS':").Layout)),
-				layout.Rigid(rowInset(labelFn(17, "").Layout)),
-				layout.Rigid(rowInset(labelFn(17, "- ARROW LEFT (←)    : MOVE THE FIGURE TO THE LEFT").Layout)),
-				layout.Rigid(rowInset(labelFn(17, "- ARROW RIGHT (→) : MOVE THE FIGURE TO THE RIGHT").Layout)),
-				layout.Rigid(rowInset(labelFn(17, "- ARROW UP (↑)         : ROTATE THE FIGURE").Layout)),
-				layout.Flexed(1, rowInset(labelFn(17, "- ARROW DOWN (↓)   : SPEED UP THE FALL OF THE FIGURE").Layout)),
-			)
+		labelFn := func(size float32, s string) material.LabelStyle { // to change the color of the text
+			style := material.Label(th, unit.Dp(size), s)
+			style.Color = color.NRGBA{A: 0xff, R: 0xff, G: 0xff, B: 0xff}
+			return style
 		}
 
-		matCloseBut := material.Button(th, &close, " Close ")
-		matCloseBut.Font = text.Font{Variant: "Mono", Weight: text.Bold, Style: text.Italic}
-
-		layout.NE.Layout(gtx, addimage.Layout) // NE (North East) is used to move the picture to the top right corner
 		d := layout.Flex{Axis: layout.Vertical}.Layout(gtx,
-			layout.Flexed(1, textRowLayout),
-			layout.Rigid(rowInset(matCloseBut.Layout)),
+			layout.Rigid(rowInset(labelFn(22, " * INSTRUCTION TO MY 'TETRIS' :").Layout)),
+			layout.Rigid(rowInset(labelFn(17, "").Layout)),
+			layout.Flexed(1, rowInset(textRowLayout)),
+			layout.Rigid(rowInset(material.Button(th, &close, "Close").Layout)),
 		)
+		layout.NE.Layout(gtx, addimage.Layout) // NE (North East) is used to move the picture to the top right corner
 		if close.Clicked() {
 			return MainMenu(th, state), d
 		}
